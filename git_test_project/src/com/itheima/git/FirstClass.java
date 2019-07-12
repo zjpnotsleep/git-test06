77fa85243899b66b3a2e8f1b83566378463850ed
zhangsan <zhangsan@itcast.cn>
2019-07-12 18:43:43 +0800

    修改

---
+++ git_test_project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,20 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+    public void test(){
+        int i = 1;
+        i = i+5;
+        System.out.println(i);
+
+    }
+}
